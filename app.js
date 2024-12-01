31e227efd8f1fdde22a99870a63c24660f8930a5
Juan Rodriguez <Juan.Rodriguez23@snhu.edu>
2024-12-01 12:00:13 +0900

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var handlebars =  require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index'); // API router
+
+var handlebars =  require('hbs');
+//Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -29,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel',  travelRouter);
+app.use('/api', apiRouter); // Mount the API routes under the '/api' base path
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
